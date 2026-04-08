3c32ddf65cffd0e0387cceca2db0ee34cf51bd34
Rogerio Almeida <rogerioja89@gmail.com>
2026-04-07 21:01:55 -0300

    Alteracao Banco de dados para h2 funcionou post

--- src/test/java/com/github/rogerioja89/GreetingResourceIT.java
+++
@@ -1,8 +0,0 @@
-package com.github.rogerioja89;
-
-import io.quarkus.test.junit.QuarkusIntegrationTest;
-
-@QuarkusIntegrationTest
-class GreetingResourceIT extends GreetingResourceTest {
-    // Execute the same tests but in packaged mode.
-}
